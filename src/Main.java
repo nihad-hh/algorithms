4bb1095baf00fb0519590ad0acdd7baa3941d235
eng-nihad <hodzicnihad10@gmail.com>
2023-09-12 18:45:51 +0200

    Added bubbleSort function and additional test cases.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
 public class Main {
@@ -63,7 +62,6 @@ public class Main {
 
         return sortedList;
     }
-
     public static List<Integer> insertionSort(List<Integer> lista) {
         for (int i = 1; i < lista.size(); i = i + 1) {
             int j = i;
@@ -76,7 +74,6 @@ public class Main {
         }
         return lista;
     }
-
     public static List<Integer> selectionSort(List<Integer> lista) {
         for (int i = 0; i < lista.size(); i = i + 1) {
             int minIndex = i;
@@ -91,9 +88,34 @@ public class Main {
         }
         return lista;
     }
+
+    public static List<Integer> bubbleSort(List<Integer> lista) {
+        for (int i = 0; i < lista.size() - 1; i = i + 1) {
+            for (int j = 0; j < lista.size() - 1 - i; j = j + 1) {
+                if (lista.get(j) > lista.get(j + 1)) {
+                    int temp = lista.get(j);
+                    lista.set(j, lista.get(j + 1));
+                    lista.set(j + 1, temp);
+                }
+            }
+        }
+        return lista;
+    }
     public static void main(String[] args) {
         ArrayList<Integer> lista = new ArrayList<>(Arrays.asList(5, 3, 15, 7, 1, 12, 4));
-        ArrayList<Integer> sortedList = new ArrayList<>(Main.selectionSort(lista));
+        ArrayList<Integer> sortedList = new ArrayList<>(Main.bubbleSort(lista));
+        System.out.println(sortedList);
+
+        lista = new ArrayList<>();
+        sortedList = new ArrayList<>(Main.bubbleSort(lista));
+        System.out.println(sortedList);
+
+        lista = new ArrayList<>(Arrays.asList(1));
+        sortedList = new ArrayList<>(Main.bubbleSort(lista));
+        System.out.println(sortedList);
+
+        lista = new ArrayList<>(Arrays.asList(1, 1, 2, 3, 1, 7));
+        sortedList = new ArrayList<>(Main.bubbleSort(lista));
         System.out.println(sortedList);
 
     }
